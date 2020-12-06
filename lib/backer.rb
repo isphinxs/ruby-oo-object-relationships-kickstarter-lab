c63e86ab102de436aea75e4c928b781bbc961395
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-05 20:56:43 -0500

    commit tree 94d6821109861a355f20e145b52e5cc73371630b

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+require "pry"
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def projects
+        ProjectBacker.all.select {|project| project.backer == self}
+    end
+
+    def backed_projects
+        self.projects.collect {|project| project.project}
+    end
+end
